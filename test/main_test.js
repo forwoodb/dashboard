ee83ddd661883bde1bb4f5004025fd33749e5f8a
Brent Forwood <forwoodb77@gmail.com>
2017-12-12 20:21:10 -0500

    Simplified .gitignore and completed test environment

---
+++ test/main_test.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+
+// Test suite
+// describe('title string', function(anonymous_function) {
+//   Test spec(unit test)
+//   it('behavior string', function() {
+//     expectation - state condition for our test
+//   });
+// });
+
+// Sanity Check
+describe('Mocha', function() {
+  it('should run our tests using npm', function() {
+    expect(true).to.be.ok;
+  })
+});
